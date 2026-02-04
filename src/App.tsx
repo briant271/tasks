09f4906751d3fef511afa05bbe0bfa6fa8f6f780
Brian Tran <briant@udel.edu>
2026-02-03 19:40:37 -0500

    Added name to App

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
+            <p>
+                This is Brian Tran, asdfasdfasdf!
+            </p>
         </div>
     );
 }
