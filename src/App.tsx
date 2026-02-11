bec0b339c5a1a757a4d77d123db6a100fa61f26a
Brian Tran <briant@udel.edu>
2026-02-11 12:36:07 -0500

    added html changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import {Button, Container, Row, Col} from 'react-bootstrap'
 import "./App.css";
 
 function App(): React.JSX.Element {
@@ -7,6 +8,10 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+
+            <h1 style={{ backgroundColor: 'teal' }}> 
+
+            </h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
@@ -15,7 +20,35 @@ function App(): React.JSX.Element {
             <p>
                 This is Brian Tran, Hello World!
             </p>
+
+            List of Stuff:
+            <ol>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ol>
+
+             <Button onClick={ () => { console.log("Hello World!") } }>Log Hello World</Button>
+             <p> </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <div style={{ backgroundColor: 'red', width:20, height:20 }}>
+
+                        </div>
+                    </Col>
+                    <Col>
+                        <div style={{ backgroundColor: 'red', width:20, height:20 }}>
+
+                        </div>
+                        
+                        <img src="/images/milesMorales.jpg" alt="Spiderman Meme" />
+                    </Col>
+                </Row>
+            </Container>
+            
         </div>
+
     );
 }
 
